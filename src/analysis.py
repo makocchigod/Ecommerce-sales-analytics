8066dc967298dd4de93a119ba6a46eaa1a27df3c
makocchigod <korzeniewskimarcin2008@gmail.com>
2026-09-11 23:41:52 +0200

    Revenue

--- src/analysis.py
+++ src/analysis.py
@@ -1,2 +1,18 @@
 from merge import merge_all
-df = merge_all()
+import matplotlib.pyplot as plt
+
+def revenue():
+    df = merge_all()
+    df['revenue'] = df['quantity'] * df['price']
+    df['month'] = df['order_date'].dt.to_period('M')
+    print(df['month'].unique())
+    result = df.groupby(['product_name','month']).agg(revenue=('revenue', 'sum'))
+    return result
+
+def monthly_revenue():
+    df = revenue()
+    result = df.groupby('month').agg(revenue=('revenue', 'sum'))
+    print(result)
+    result['revenue'].plot(kind='line', ylabel='revenue', xlabel='month')
+    plt.show()
+monthly_revenue()
